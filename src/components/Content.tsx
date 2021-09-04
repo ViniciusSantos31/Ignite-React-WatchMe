0aa9d02b820f8e8d28c6b2c46816650a1525b9ff
Vinicius Santos <viniciussilveira@mat.ci.ufpb.br>
2021-09-04 03:04:41 -0300

    componentizing the application

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,59 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Content {
+  selectedGenreId: number;
+  selectedGenre: {
+    id: number;
+    title: string;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  };
+}
+
+export function Content(props: Content) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [props.selectedGenreId]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
